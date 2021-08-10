68a084263c8660dac90a3dd5c518d1ac520599ef
Matheus Galvão <matheusgcorreia@gmail.com>
2021-08-10 14:48:18 -0300

    corrigindo exceções em autenticação

--- src/actions/auth.action.ts
+++ src/actions/auth.action.ts
@@ -3,6 +3,7 @@ import { sign } from 'jsonwebtoken';
 import configs from "../configs/configs";
 import BadRequestError from "../exceptions/bad-request.error";
 import AuthResponser from "./responsers/auth.responser";
+import NotFoundError from "../exceptions/not-found.error";
 
 export default class AuthAction {
   public constructor(
@@ -10,18 +11,26 @@ export default class AuthAction {
   ) {}
 
   async authenticate(username: string, sentPassword: string) {
-    const { password, role } = await this.userRepository.get(username, true);
+    try {
+      const { password, role } = await this.userRepository.get(username, true);
+  
+      if(password !== sentPassword) {
+        throw new BadRequestError('Credenciais inválidas');
+      }
+  
+      const token = sign(
+        { role, username }, 
+        configs.jwtSecret(), 
+        { expiresIn: configs.jwtTtl() }
+      );
+      
+      return AuthResponser.successResponse(token);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw new BadRequestError('Credenciais inválidas');
+      }
 
-    if(password !== sentPassword) {
-      throw new BadRequestError('Credenciais inválidas');
+      throw error;
     }
-
-    const token = sign(
-      { role, username }, 
-      configs.jwtSecret(), 
-      { expiresIn: configs.jwtTtl() }
-    );
-    
-    return AuthResponser.successResponse(token);
   }
 }
